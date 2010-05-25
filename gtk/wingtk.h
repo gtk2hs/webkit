98f1ba0f0f24ec959dc8e1ab1f1cdb4ca6db481d
Axel.Simon <Axel.Simon@in.tum.de>
2010-05-25 21:06:36 +0000

    Remove all but webkit.

--- gtk/wingtk.h
+++
@@ -1,5 +0,0 @@
-#include <gtk/gtk.h>
-/* these are necessary on Win32 to circumvent the strcuture size check */
-#undef gtk_init_check
-#undef gtk_init
-
--- tools/win32/win32-build-installer.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-. ./win32-build.conf
-
-"${INNOSETUP_BIN_DIR}/iscc.exe" ${INSTALL_SOURCE_DIR}/gtk2hs.iss
