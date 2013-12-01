196e405b1b243f1e08400691bb38c976b5eff9a8
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2013-12-01 21:42:37 +1300

    Build both GTK+2 and GTK+3 packages on travis

--- install-both.sh
+++ install-both.sh
@@ -3,10 +3,10 @@
 cabal clean
 mv webkit.cabal-renamed webkit.cabal || true
 mv webkitgtk3.cabal webkitgtk3.cabal-renamed || true
-cabal-src-install "$@"
+cabal install "$@"
 
 cabal clean
 mv webkitgtk3.cabal-renamed webkitgtk3.cabal || true
 mv webkit.cabal webkit.cabal-renamed || true
-cabal-src-install "$@"
+cabal install "$@"
 
